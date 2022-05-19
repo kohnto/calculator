567b581c3726d4d24b8f9abaea186d334e2e61a2
Tobias Kohn <tobias.kohn@MacBook-Pro.local>
2022-05-19 12:40:21 +0300

    define components

---
+++ src/components/ButtonBlock.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <v-btn> {{button_text}}</v-btn>
+
+      
+  </div>
+</template>
+
+<script>
+export default {
+    props:[
+        'button_text'
+    ],
+    data() {
+        return {
+            button_text:this.button_text
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import ButtonBlock from './components/ButtonBlock';
+import GridBlock from './components/GridBlock'
+
+import RowBlock from './components/RowBlock'
+import TextBlock from './components/TextBlock'
+
 
 Vue.config.productionTip = false
 
-new Vue({
+var vm = new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.component('button-block',ButtonBlock);
+
+
